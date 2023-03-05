032f197bae8da7ee658a97b5f196f3ccfef26315
Parker Place <pleeplace@gmail.com>
2023-03-05 12:12:23 -0700

    remove .nev

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,7 +71,7 @@ const inter = Inter({ subsets: ["latin"] });
 function createPromisesArray(inputArr) {
   // create a new array to hold the Promises
   const promises = [];
-
+  console.log("openai~", configuration);
   // loop through each value in the input array
   for (let i = 0; i < inputArr.length; i++) {
     // create a new Promise that resolves with the current value
@@ -100,74 +100,80 @@ export async function getServerSideProps({ req, res }) {
   );
   const allRequests = createPromisesArray(summaries);
   // return;
-  const allResponses = await Promise.all(allRequests);
-  // console.log("ALl Requests", allResponses);
-  const allResponsesData = allResponses.map((response, i) => {
+  try {
+    const allResponses = await Promise.all(allRequests);
+
+    // console.log("ALl Requests", allResponses);
+    const allResponsesData = allResponses.map((response, i) => {
+      return {
+        // @ts-ignore
+        chatgtpRes: response?.data?.choices[0]?.text,
+        proposal: proposals.proposals[i],
+      };
+    });
+
+    // const allResponsesData = [
+    //   {
+    //     chatgtpRes:
+    //       "\n" +
+    //       "\n" +
+    //       "- Propose the creation of a Decentralized Autonomous Organization (DAO)\n" +
+    //       "- Governed by a smart contract on a blockchain, ensuring consensus and transparency\n" +
+    //       "- Prioritize community involvement and collaboration with diverse perspectives\n" +
+    //       "- Focus on sustainability and social impact with clear guidelines and protocols for decision-making and security",
+    //     proposal: {
+    //       id: "0x12345...",
+    //       dao: {
+    //         address: "0x1234567890123456789012345678901234567890",
+    //         name: "Cool DAO",
+    //       },
+    //       creatorAddress: "0x1234567890123456789012345678901234567890",
+    //       metadata: {
+    //         title: "Test Proposal",
+    //         summary: `We are proposing the creation of a Decentralized Autonomous Organization (DAO) to foster community-driven decision-making and collaboration. Our vision is to establish a platform where members can participate in a transparent, democratic process to fund and execute projects that align with our shared values and goals.
+
+    //         The DAO will be governed by a smart contract on a blockchain, ensuring that decisions are made through consensus and that all transactions are transparent and immutable. Members will be able to vote on proposals and proposals can be submitted by anyone. Once a proposal is approved, funds will be allocated from the DAO's treasury to support the project.
+
+    //         Our DAO will be unique in that it will prioritize community involvement and collaboration. We believe that diverse perspectives lead to better decision-making and that everyone should have a voice. Therefore, we will prioritize outreach efforts to ensure that members from all backgrounds are represented and that their ideas are heard.
+
+    //         In addition, our DAO will have a strong focus on sustainability and social impact. We will prioritize proposals that have a positive impact on society and the environment, while also promoting long-term financial stability.
+
+    //         To ensure the success of our DAO, we will establish clear guidelines and protocols for decision-making, including a code of conduct and a dispute resolution process. We will also prioritize security and auditability, to ensure that the DAO remains secure and transparent.
+
+    //         In summary, our DAO will:
+
+    //         Foster community-driven decision-making and collaboration
+    //         Be governed by a transparent and democratic process
+    //         Prioritize outreach efforts to ensure diverse representation
+    //         Have a strong focus on sustainability and social impact
+    //         Establish clear guidelines and protocols for decision-making
+    //         Prioritize security and auditability
+    //         We are excited to establish this DAO and look forward to building a strong and inclusive community. Thank you for your consideration.`,
+    //       },
+    //       startDate: 1678002739304,
+    //       endDate: 1678013839304,
+    //       status: "Executed",
+    //       token: {
+    //         address: "0x1234567890123456789012345678901234567890",
+    //         name: "The Token",
+    //         symbol: "TOK",
+    //         decimals: 18,
+    //       },
+    //       results: {
+    //         yes: 100000,
+    //         no: 77777,
+    //         abstain: 0,
+    //       },
+    //     },
+    //   },
+    // ];
     return {
-      // @ts-ignore
-      chatgtpRes: response?.data?.choices[0]?.text,
-      proposal: proposals.proposals[i],
+      props: { data: allResponsesData },
     };
-  });
-  // const allResponsesData = [
-  //   {
-  //     chatgtpRes:
-  //       "\n" +
-  //       "\n" +
-  //       "- Propose the creation of a Decentralized Autonomous Organization (DAO)\n" +
-  //       "- Governed by a smart contract on a blockchain, ensuring consensus and transparency\n" +
-  //       "- Prioritize community involvement and collaboration with diverse perspectives\n" +
-  //       "- Focus on sustainability and social impact with clear guidelines and protocols for decision-making and security",
-  //     proposal: {
-  //       id: "0x12345...",
-  //       dao: {
-  //         address: "0x1234567890123456789012345678901234567890",
-  //         name: "Cool DAO",
-  //       },
-  //       creatorAddress: "0x1234567890123456789012345678901234567890",
-  //       metadata: {
-  //         title: "Test Proposal",
-  //         summary: `We are proposing the creation of a Decentralized Autonomous Organization (DAO) to foster community-driven decision-making and collaboration. Our vision is to establish a platform where members can participate in a transparent, democratic process to fund and execute projects that align with our shared values and goals.
-
-  //         The DAO will be governed by a smart contract on a blockchain, ensuring that decisions are made through consensus and that all transactions are transparent and immutable. Members will be able to vote on proposals and proposals can be submitted by anyone. Once a proposal is approved, funds will be allocated from the DAO's treasury to support the project.
-
-  //         Our DAO will be unique in that it will prioritize community involvement and collaboration. We believe that diverse perspectives lead to better decision-making and that everyone should have a voice. Therefore, we will prioritize outreach efforts to ensure that members from all backgrounds are represented and that their ideas are heard.
-
-  //         In addition, our DAO will have a strong focus on sustainability and social impact. We will prioritize proposals that have a positive impact on society and the environment, while also promoting long-term financial stability.
-
-  //         To ensure the success of our DAO, we will establish clear guidelines and protocols for decision-making, including a code of conduct and a dispute resolution process. We will also prioritize security and auditability, to ensure that the DAO remains secure and transparent.
-
-  //         In summary, our DAO will:
-
-  //         Foster community-driven decision-making and collaboration
-  //         Be governed by a transparent and democratic process
-  //         Prioritize outreach efforts to ensure diverse representation
-  //         Have a strong focus on sustainability and social impact
-  //         Establish clear guidelines and protocols for decision-making
-  //         Prioritize security and auditability
-  //         We are excited to establish this DAO and look forward to building a strong and inclusive community. Thank you for your consideration.`,
-  //       },
-  //       startDate: 1678002739304,
-  //       endDate: 1678013839304,
-  //       status: "Executed",
-  //       token: {
-  //         address: "0x1234567890123456789012345678901234567890",
-  //         name: "The Token",
-  //         symbol: "TOK",
-  //         decimals: 18,
-  //       },
-  //       results: {
-  //         yes: 100000,
-  //         no: 77777,
-  //         abstain: 0,
-  //       },
-  //     },
-  //   },
-  // ];
-  const data = "";
-  return {
-    props: { data: allResponsesData },
-  };
+  } catch (e) {
+    console.log("~~", e.message);
+    return { props: {} };
+  }
 }
 
 export default function Home({ data }) {
